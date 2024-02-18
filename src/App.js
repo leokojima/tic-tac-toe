01a6a841d9518ff2694798ad3064d864cd660551
leokojima <leokojima92@gmail.com>
2024-02-18 20:38:38 -0300

    you are at move #

--- src/App.js
+++ src/App.js
@@ -23,7 +23,9 @@ export default function Game() {
 
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        <button onClick={() => jumpTo(move)}>
+          {move == currentMove ? 'You are at move #' + move : description}
+        </button>
       </li>
     );
   });
