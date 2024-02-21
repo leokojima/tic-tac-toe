48f1f3d011a518b05f966955255928428b19f79f
leokojima <leokojima92@gmail.com>
2024-02-20 23:55:14 -0300

    Board component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ export default function App() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
+  const winner = calculateWinner(currentSquares);
+
+  let status;
+  if(winner) {
+    status = 'Winner: ' + winner;
+  } else if(currentMove === currentSquares.length) {
+    status = 'DRAW!';
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+  }
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
@@ -13,8 +28,14 @@ export default function App() {
     setCurrentMove(nextHistory.length - 1);
   }
 
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
+  function handleClick(i) {
+    if(currentSquares[i] || winner) {
+      return;
+    }
+
+    const nextSquares = currentSquares.slice();
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+    handlePlay(nextSquares);
   }
 
   const moves = history.map((squares, move) => {
@@ -27,11 +48,34 @@ export default function App() {
   return (
     <div className="game">
       <div className="game-board">
-        <Board currentMove={currentMove} squares={currentSquares} onPlay={handlePlay} />
+        <Board status={status} squares={currentSquares} onClick={handleClick} />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const winSituations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for(let i = 0; i < winSituations.length; i++) {
+    const [a, b, c] = winSituations[i];
+
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
 }
